97bfad3488536ddcc6ad0e02e55fb68caffa1926
BrianEtemesi <etemecci@gmail.com>
2023-02-06 12:46:28 +0000

    add comments to code

--- tests/1-insertion_sort_list.c
+++ tests/1-insertion_sort_list.c
@@ -1,13 +1,14 @@
 #include "sort.h"
 
 /**
- * _swap - swaps two elements in a linked list
+ * _swap - swaps two elements in a linked list,
  * @temp: pointer to node to be swapped
  * @list: list
  */
 
 void _swap(listint_t *temp, listint_t **list)
 {
+	/* declare variable to save temp-next, temp-prev and temp-prev-prev */
 	listint_t *x, *y, *z;
 	int a, b;
 
@@ -17,21 +18,27 @@ void _swap(listint_t *temp, listint_t **list)
 	x = temp->next;
 	y = temp->prev;
 	z = temp->prev->prev;
-
+	/* compare n to (n-1), swap only if condition is true */
 	if (a < b)
 	{
 		y->next = x;
+		/* check if temp-next is null; case for when temp is the last node of the linked list */
 		if (x)
 			x->prev = y;
 		temp->next = y;
+		/* check if temp-prev is null; case for when temp-prev is the 1st node of the list  */
 		if (y->prev == NULL)
 			*list = temp;
 		y->prev = temp;
 
 		if (z == NULL)
 		{
+			/* if z is null, temp prev will not point to anything */
 			temp->prev = NULL;
 			print_list(*list);
+			/* if temp-prev is null, it means temp has occupied the first position of the linked list
+			 * no more swapping can be done so we exit the swap function
+			 */
 			return;
 		}
 		else
@@ -40,7 +47,7 @@ void _swap(listint_t *temp, listint_t **list)
 			z->next = temp;
 			print_list(*list);
 		}
-
+		/* call swap recursively to ensure that temp compares to all the previous nodes of the linked list */
 		_swap(temp, list);
 	}
 }
@@ -54,11 +61,16 @@ void insertion_sort_list(listint_t **list)
 	listint_t *temp, *j;
 
 	temp = *list;
+	/* move temp to second element of list for easy comparison with (n-1) */
 	temp = temp->next;
 	printf("Data at temp: %d\n", temp->n);
+	/* loop through the nodes */
 	while (temp)
 	{
-		j = temp->next;
+		j = temp->next; /* save address of temp-next since temp is manipulated inside the swap function */
+		/* call swap - which will only swap if temp data is less than data at temp prev.Otherwise
+		 * it will return to the while loop for the while loop to check the next node.
+		 * */
 		_swap(temp, list);
 		temp = j;
 	}
